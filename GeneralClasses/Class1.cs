708dd535e9fa3298d56d601881297831dd807e70
Misha <student@K202-11>
2017-10-04 20:01:18 +0300

    Создан репозиторий

--- GeneralClasses/Class1.cs
+++ GeneralClasses/Class1.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GeneralsClient
+namespace GeneralClasses
 {
-    class Program
+    public class Class1
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
