814cdf57df46f33f93076d3b9f2a60b763efd261
student <student@K202-09>
2017-10-19 19:56:57 +0300

    commit

--- GeneralClasses/StaticConstats.cs
+++ GeneralClasses/StaticConstats.cs
@@ -6,13 +6,17 @@ using System.Threading.Tasks;
 
 namespace GeneralClasses
 {
-    public class StaticConstats
+    public static class StaticConstats
     {
         public static int PriceOfScientists { get; } = 100;
         public static int PriceOfSoldiers { get; } = 100;
         public static int PriceOfSeedsBuy { get; } = 20;
         public static int PriceOfSeedsSell { get; } = 15;
-       
-
+        public static int Teritory { get; } = 1000;
+        public static int CountOfPlayers { get; } = 4;
+        public static int StartCountOfSeed { get; } = 200;
+        public static int StartBalance { get; } = 10000;
+        public static int StartPeasants { get; } = 1000;
+        public static int SpecificDensity { get; } = 10;
     }
 }
